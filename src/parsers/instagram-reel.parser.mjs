ff92bd2cf8295a5e97fa95cac1a402c909f0d399
VirtualPirate <artaza13020artaza@gmail.com>
2023-05-16 12:34:44 +0530

    Reverted back to simple axios request

--- src/parsers/instagram-reel.parser.mjs
+++ src/parsers/instagram-reel.parser.mjs
@@ -3,15 +3,7 @@ import * as cheerio from "cheerio";
 
 export async function getReelInfo(url) {
   // Get the raw HTML
-  const rawHTML = (
-    await axios.get(url, {
-      maxRedirects: 0,
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
-      },
-    })
-  ).data;
+  const rawHTML = (await axios.get(url)).data;
 
   // calls cheerio to process the html received
   const $ = cheerio.load(rawHTML);
